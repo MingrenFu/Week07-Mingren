927554613e6bc1ff84200dff19b403c8f08d2d47
MingrenFu <91994074+MingrenFu@users.noreply.github.com>
2023-03-21 15:20:13 -0400

    Updated version with save function

---
+++ PhotoEditingApp/PhotoManipulation/PhotoManipulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManipulationApp.swift
+//  PhotoManipulation
+//
+//  Created by Michael Fu on 3/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManipulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
